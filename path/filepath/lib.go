4c1c7b6f2a300201017398f91a44cfce2d523941
tzz <283853646@qq.com>
2021-01-28 11:57:31 +0800

    Add filepath

---
+++ path/filepath/lib.go
@@ -0,0 +1,11 @@
+package filepath
+
+import (
+	std "path/filepath"
+)
+
+// 和 stdpath 中不一的是, 不会去 Clean
+func DirPath(filepath string) string {
+	dir, _ := std.Split(filepath)
+	return dir
+}
--- path/lib.go
+++ path/lib.go
@@ -1,11 +1,1 @@
 package path
-
-import (
-	stdpath "path"
-)
-
-// 和 stdpath 中不一的是, 不会去 Clean
-func DirPath(filepath string) string {
-	dir, _ := stdpath.Split(filepath)
-	return dir
-}
