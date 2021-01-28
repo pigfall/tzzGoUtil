20b3cb42cf9930d71d283be64469a5673e927bf4
tzz <283853646@qq.com>
2021-01-28 17:22:02 +0800

    Add: 打开 文件浏览器功能

---
+++ file_explorer/lib_linux.go
@@ -0,0 +1,14 @@
+package file_explorer
+
+import (
+	"fmt"
+	"github.com/Peanuttown/tzzGoUtil/process"
+)
+
+func OpenExplorer(path string) error {
+	return fmt.Errorf("unsupported")
+}
+
+func SupportOpenExplorer() bool {
+	return false
+}
---
+++ file_explorer/lib_windows.go
@@ -0,0 +1,23 @@
+package file_explorer
+
+import (
+	"fmt"
+	"github.com/Peanuttown/tzzGoUtil/process"
+	//"path/filepath"
+	"strings"
+)
+
+// path format : C:/Users
+func OpenExplorer(path string) error {
+	fmt.Println(strings.ReplaceAll(path, "/", "\\"))
+	//_, errOut, err := process.ExeOutput("powershell.exe", "-c", fmt.Sprintf("(cd %s) -or (explorer.exe .)", path))
+	_, errOut, err := process.ExeOutput("explorer.exe", strings.ReplaceAll(path, "/", "\\"))
+	if err != nil {
+		return fmt.Errorf("%w,%s", err, errOut)
+	}
+	return nil
+}
+
+func SupportOpenExplorer() bool {
+	return true
+}
