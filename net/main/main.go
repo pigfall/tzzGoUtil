559d811b0ca8ebb04e771eefe34991abff7500ee
tzz <283853646@qq.com>
2021-03-31 22:38:47 +0800

    update

--- net/main/main.go
+++ net/main/main.go
@@ -1,9 +1,35 @@
 package main
 
-import(
-    "github.com/Peanuttown/tzzGoUtil/net"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	//"github.com/Peanuttown/tzzGoUtil/terminal/ansi"
+	"io"
+	"os/exec"
 )
 
-func main(){
-    net.Listen("etho")
+func main() {
+	cmd := exec.Command("you-get.exe", "-f", "https://www.bilibili.com/video/BV1Tt411776s?from=search&seid=9333383915621586500")
+	rd, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	err = cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+	defer rd.Close()
+	bufReader := bufio.NewReaderSize(rd, 2048)
+	for {
+		line, err := bufReader.ReadBytes('%')
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				return
+			}
+			panic(err)
+		}
+		fmt.Println(string(line[len(line)-5:]))
+	}
+	cmd.Wait()
 }
