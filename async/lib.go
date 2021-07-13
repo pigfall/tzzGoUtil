2b9c76e12ce006b32b4c79f0dc688ca92035ca28
tzz <tzz.wannamoney@qq.com>
2021-07-13 14:03:22 +0800

    Add: async

--- async/lib.go
+++ async/lib.go
@@ -13,3 +13,14 @@ func AsyncDo(ctx context.Context,wg *sync.WaitGroup,do func(ctx context.Context)
 		do(ctx)
 	}()
 }
+
+func  AsyncDoWithCancel(ctx context.Context,wg *sync.WaitGroup,do func(ctx context.Context),cancel context.CancelFunc){
+	wg.Add(1)
+	go func(ctx context.Context){
+		defer func(){
+			cancel()
+			wg.Done()
+		}()
+		do(ctx)
+	}(ctx)
+}
