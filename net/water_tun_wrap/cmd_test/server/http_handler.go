c96e7e7f2e834e995212574cdf02685566e14fbd
tzz <koi_town@outlook.com>
2021-09-19 12:31:56 +0000

    tmp

---
+++ net/water_tun_wrap/cmd_test/server/http_handler.go
@@ -0,0 +1,102 @@
+package main
+
+import(
+	"context"
+	"sync"
+		"net/http"
+	"github.com/Peanuttown/tzzGoUtil/net/water_tun_wrap/cmd_test/utils"	
+
+	"github.com/Peanuttown/tzzGoUtil/log"
+	"github.com/Peanuttown/tzzGoUtil/async"
+	ws "github.com/gorilla/websocket"
+)
+
+
+
+func NewConnHandler(logger log.LoggerLite,ctx context.Context,msgReadFromTun chan utils.MsgReadFromTun,msgWillWriteToTun chan utils.MsgWillWriteToTun)http.Handler{
+	return &ConnHandler{
+		logger:logger,
+		ctx:ctx,
+		msgReadFromTun:msgReadFromTun,
+		msgWillWriteToTun:msgWillWriteToTun,
+	}
+
+}
+
+
+type ConnHandler struct{
+	logger log.LoggerLite
+	ctx context.Context
+	msgReadFromTun chan utils.MsgReadFromTun
+	msgWillWriteToTun chan utils.MsgWillWriteToTun
+}
+
+
+func (this *ConnHandler)  ServeHTTP(w http.ResponseWriter, req *http.Request){
+	logger := this.logger
+	logger.Info("rcv one http request")
+	upgrader := ws.Upgrader{}
+	conn,err := upgrader.Upgrade(w,req,nil)
+	if err != nil{
+		logger.Error("upgrad to web socket failed:",err)
+		return
+	}
+	defer conn.Close()
+	wg := sync.WaitGroup{}
+	msgWillWriteToConn :=make(chan []byte,100)
+	msgReadFromConn := make(chan []byte,100)
+	async.AsyncDo(
+		this.ctx,
+		&wg,
+		func(ctx context.Context){
+			utils.LoopReadMsgFromConn(conn,logger,msgReadFromConn)
+		},
+	)
+	async.AsyncDo(
+		this.ctx,
+		&wg,
+		func(ctx context.Context){
+			utils.LoopWriteMsgToConn(ctx,logger,conn,msgWillWriteToConn)
+		},
+			
+	)
+	async.AsyncDo(
+		this.ctx,
+		&wg,
+		func(ctx context.Context){
+			<-ctx.Done()
+			conn.Close()
+			close(msgWillWriteToConn)
+		},
+	)
+	async.AsyncDo(
+		this.ctx,
+		&wg,
+		func(ctx context.Context){
+			utils.HandleDataFromConn(
+				ctx,
+				logger,
+				msgReadFromConn,
+				this.msgWillWriteToTun,
+
+			)
+		},
+	)
+	async.AsyncDo(
+		this.ctx,
+		&wg,
+		func(ctx context.Context){
+			utils.HandleDataFromTun(
+				ctx,
+				logger,
+				this.msgReadFromTun,
+				msgWillWriteToConn,
+			)
+		},
+	)
+	wg.Wait()
+}
+
+
+
+ 
---
+++ net/water_tun_wrap/cmd_test/server/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"os/signal"
+	"syscall"
+	"sync"
+	"context"
+	"os"
+	"net/http"
+	"net"
+"github.com/Peanuttown/tzzGoUtil/log"		
+"github.com/Peanuttown/tzzGoUtil/async"		
+	"github.com/Peanuttown/tzzGoUtil/net/water_tun_wrap/cmd_test/utils"	
+)
+
+
+func main() {
+	var logger log.LoggerLite= log.NewLogger()
+	// {  parse params
+	var listenAt string
+	flag.StringVar(&listenAt,"listenAt","","listen at")
+	flag.Parse()
+	if  len(listenAt) == 0{
+		listenAt = ":10101"
+	}
+	// }
+
+	ctx,cancelCtx := context.WithCancel(context.Background())
+	defer cancelCtx()
+	var cancelFuncs = []func(){func(){cancelCtx()}}
+	wg := sync.WaitGroup{}
+
+	// {　create tun interface
+	tun,err := createTun("172.16.0.1/16",logger)
+	if err != nil{
+		logger.Error(err)
+		os.Exit(1)
+	}
+  // }
+	// { // listen port
+	listener,err := net.Listen("tcp",listenAt)
+	if err != nil{
+		logger.Error(err)
+		os.Exit(1)
+	}
+	cancelFuncs = append(cancelFuncs,func(){listener.Close()})
+	// }
+
+	var msgReadFromTun = make(chan utils.MsgReadFromTun,100)
+	var msgWillWriteToTun = make(chan utils.MsgWillWriteToTun,100)
+	async.AsyncDo(
+		ctx,
+		&wg,
+		func(ctx context.Context){
+			utils.LoopRWTun(ctx,logger,tun,msgReadFromTun,msgWillWriteToTun)
+		},
+	)
+
+	// {　start server
+	async.AsyncDo(
+		ctx,
+		&wg,
+		func(ctx context.Context){
+			svr := http.NewServeMux()
+			svr.Handle("/",NewConnHandler(logger,ctx,msgReadFromTun,msgWillWriteToTun))
+			err :=http.Serve(listener,svr)
+			if err != nil{
+				logger.Error(err)
+			}
+		},
+	)
+	// }
+	wg.Wait()
+}
+
+
+
+func elegantWaitAndQuit(ctx context.Context,logger log.LoggerLite,cancel func(),wg *sync.WaitGroup,taskOver chan struct{}){
+	sig := make(chan os.Signal ,1)
+	signal.Notify(sig,syscall.SIGTERM,syscall.SIGINT)
+	select{
+	case <-taskOver:
+		logger.Info("All async tasks over")
+		break
+	case <-ctx.Done():
+		logger.Info("Context Done")
+		break
+	case <-sig:
+		logger.Info("Rcv quit signal")
+	}
+	logger.Infof("Cancel context, app will quit")
+	cancel()
+	logger.Infof("Wait all task done")
+	wg.Wait()
+	logger.Info("Quit app")
+}
