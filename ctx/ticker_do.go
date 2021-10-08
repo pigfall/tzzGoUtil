8585e1f3dc8074dc011770ac3cc102bc32347e30
tzz <koi_town@outlook.com>
2021-10-08 13:10:36 +0000

    Add ticker do

---
+++ ctx/ticker_do.go
@@ -0,0 +1,25 @@
+package ctx
+
+import (
+
+	"time"
+	"context"
+		
+)
+
+
+func TickerDo(ctx context.Context,tickerDuration time.Duration,do func()error)(error){
+	ticker := time.NewTicker(tickerDuration)
+	defer ticker.Stop()
+	for{
+		select{
+		case  <-ticker.C:
+			err := do()
+			if err != nil{
+				return err
+			}
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
