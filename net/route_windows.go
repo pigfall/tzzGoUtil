cb1161be9cbfee5efe6b57920f0ab30817d4bfcb
tzz <koi_town@outlook.com>
2021-10-14 11:48:06 +0800

    Add: windows route

---
+++ net/route_windows.go
@@ -0,0 +1,27 @@
+package net
+
+import(
+		"net"
+)
+
+func AddRoute(target net.IP,devName string,via net.IP)error{
+	panic("TODO")
+}
+
+func AddRouteIpNet(target *IpWithMask,devName string,via net.IP)error{
+	panic("TODO")
+}
+
+func DelRoute(target net.IP)error{
+	panic("TODO")
+}
+
+
+func GetDefaultRouteRule()(*RouteRule,error){
+	panic("TODO")
+}
+
+func RouteList()([]*RouteRule,error){
+	panic("TODO")
+}
+
--- net/wintun/examples/main.go
+++ net/wintun/examples/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"runtime/debug"
 	"time"
+	"log"
 	"fmt"
 	"github.com/pigfall/tzzGoUtil/net/wintun"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket"
 )
 
 func main() {
+	defer func(){
+		e := recover()
+		if e != nil {
+			fmt.Println(e)
+			debug.PrintStack()
+			time.Sleep(time.Hour)
+		}
+		time.Sleep(time.Second*100)
+	}()
 	tunIfce,err := wintun.NewTun("tzzTest",1500)
 	if err != nil{
 		panic(err)
@@ -14,7 +27,22 @@ func main() {
 	fmt.Println(tunIfce)
 	err = tunIfce.SetIp("192.168.2.1/24")
 	if err != nil{
-		fmt.Println(err)
+		panic(err)
 	}
-	time.Sleep(time.Hour)
+	var buf = make([]byte,1024*4)
+	fmt.Println("Reading")
+	for{
+		n,err :=tunIfce.Read(buf)
+		if err != nil{
+			panic(err)
+		}
+		//log.Println(string(buf[:n]))
+		pac := gopacket.NewPacket(buf[:n],layers.LayerTypeEthernet,gopacket.Default)
+		if ethLayer := pac.Layer(layers.LayerTypeEthernet);ethLayer!=nil{
+			log.Println("ethernet packet ")
+		}else{
+			log.Println("not ethernet packet")
+		}
+
+	}
 }
